49075fa79aa43fd224e405aa4ac1756bc66a25b5
unknown <murtaza.zoaib@connecsi.com>
2018-11-26 23:33:13 +0500

    Fixed menu bar

--- static/app-assets/js/app-sidebar.js
+++ static/app-assets/js/app-sidebar.js
@@ -17,15 +17,16 @@
     $sidebar_img_container = $('.sidebar-background'),
     $wrapper = $('.wrapper');
 
-    $sidebar_content.perfectScrollbar();
+    // $sidebar_content.perfectScrollbar();
 
     if( $sidebar_img_container.length !== 0 && $sidebar_img !== undefined ){
         $sidebar_img_container.css('background-image','url("' + $sidebar_img + '")');
     }
 
-    if(!$wrapper.hasClass('nav-collapsed')){
-        $sidebar_content.find('li.active').parents('li').addClass('open');
-    }
+    // if(!$wrapper.hasClass('nav-collapsed')){
+    //     $sidebar_content.find('li.active').parents('li').addClass('open');
+    //     console.log('collapse2');
+    // }
 
 
     // Match the height of each card in a row
@@ -36,92 +37,101 @@
     },500);
 
 
-    $sidebar_content.on('click', '.navigation li a',function(){
-        var $this = $(this),
-        listItem = $this.parent('li');
+    // $sidebar_content.on('click', '.navigation li a',function(){
+    //     var $this = $(this),
+    //     listItem = $this.parent('li');
+    //
+    //     if(listItem.hasClass('has-sub') && listItem.hasClass('open')){
+    //         collapse(listItem);
+    //         console.log('collapse3');
+    //     }
+    //     else{
+    //         if(listItem.hasClass('has-sub')){
+    //             expand(listItem);
+    //         }
+    //
+    //         // If menu collapsible then do not take any action
+    //         if ($sidebar_content.data('collapsible')) {
+    //             return false;
+    //         }
+    //         // If menu accordion then close all except clicked once
+    //         else {
+    //             openListItems = listItem.siblings('.open');
+    //             collapse(openListItems);
+    //             console.log('collapse4');
+    //             listItem.siblings('.open').find('li.open').removeClass('open');
+    //             console.log('collapse5');
+    //
+    //         }
+    //     }
+    // });
 
-        if(listItem.hasClass('has-sub') && listItem.hasClass('open')){
-            collapse(listItem);
-        }
-        else{
-            if(listItem.hasClass('has-sub')){
-                expand(listItem);
-            }
+    // function collapse($listItem, callback) {
+    //     var $subList = $listItem.children('ul');
+    //
+    //     $subList.show().slideUp(200, function() {
+    //         $(this).css('display', '');
+    //
+    //         $(this).find('> li').removeClass('is-shown');
+    //
+    //         $listItem.removeClass('open');
+    //         console.log('collapse6');
+    //
+    //         if (callback) {
+    //             callback();
+    //         }
+    //     });
+    //
+    // }
 
-            // If menu collapsible then do not take any action
-            if ($sidebar_content.data('collapsible')) {
-                return false;
-            }
-            // If menu accordion then close all except clicked once
-            else {
-                openListItems = listItem.siblings('.open');
-                collapse(openListItems);
-                listItem.siblings('.open').find('li.open').removeClass('open');
-            }
-        }
-    });
+    // function expand($listItem, callback) {
+    //     var $subList = $listItem.children('ul');
+    //     var $children = $subList.children('li').addClass('is-hidden');
+    //
+    //     $listItem.addClass('open');
+    //     console.log('collapse7');
+    //
+    //     $subList.hide().slideDown(200, function() {
+    //         $(this).css('display', '');
+    //
+    //         if (callback) {
+    //             callback();
+    //         }
+    //     });
+    //
+    //
+    //
+    //     setTimeout(function() {
+    //         $children.addClass('is-shown');
+    //         $children.removeClass('is-hidden');
+    //     }, 0);
+    // }
 
-    function collapse($listItem, callback) {
-        var $subList = $listItem.children('ul');
-
-        $subList.show().slideUp(200, function() {
-            $(this).css('display', '');
-
-            $(this).find('> li').removeClass('is-shown');
-
-            $listItem.removeClass('open');
-
-            if (callback) {
-                callback();
-            }
-        });
-
-    }
-
-    function expand($listItem, callback) {
-        var $subList = $listItem.children('ul');
-        var $children = $subList.children('li').addClass('is-hidden');
-
-        $listItem.addClass('open');
-
-        $subList.hide().slideDown(200, function() {
-            $(this).css('display', '');
-
-            if (callback) {
-                callback();
-            }
-        });
-
-        
-
-        setTimeout(function() {
-            $children.addClass('is-shown');
-            $children.removeClass('is-hidden');
-        }, 0);
-    }
-
-    $('.logo-text').on('click',function(){
-
-        var listItem = $sidebar_content.find('li.open.has-sub'),
-        activeItem = $sidebar_content.find('li.active');
-
-        if(listItem.hasClass('has-sub') && listItem.hasClass('open')){
-            collapse(listItem);
-            listItem.removeClass('open');
-            if(activeItem.closest('li.has-sub')){
-                openItem = activeItem.closest('li.has-sub');
-                expand(openItem);
-                openItem.addClass('open');
-            }
-        }
-        else{
-            if(activeItem.closest('li.has-sub')){
-                openItem = activeItem.closest('li.has-sub');
-                expand(openItem);
-                openItem.addClass('open');
-            }
-        }
-    });
+    // $('.logo-text').on('click',function(){
+    //
+    //     var listItem = $sidebar_content.find('li.open.has-sub'),
+    //         console.log('collapse');
+    //     activeItem = $sidebar_content.find('li.active');
+    //
+    //     if(listItem.hasClass('has-sub') && listItem.hasClass('open')){
+    //         collapse(listItem);
+    //         console.log('collapse');
+    //         listItem.removeClass('open');
+    //         if(activeItem.closest('li.has-sub')){
+    //             openItem = activeItem.closest('li.has-sub');
+    //             expand(openItem);
+    //             openItem.addClass('open');
+    //         }
+    //     }
+    //     else{
+    //         if(activeItem.closest('li.has-sub')){
+    //             openItem = activeItem.closest('li.has-sub');
+    //             expand(openItem);
+    //             openItem.addClass('open');
+    //             console.log('collapse');
+    //         }
+    //     }
+    // });
 
 
     $('.nav-toggle').on('click',function(){
@@ -153,8 +163,10 @@
     $sidebar.on('mouseenter', function() {
         if($wrapper.hasClass('nav-collapsed')){
             $wrapper.removeClass('menu-collapsed');
+            $('#logo2').show();
             var $listItem = $('.navigation li.nav-collapsed-open'),
             $subList = $listItem.children('ul');
+            console.log('collapse8');
 
             $subList.hide().slideDown(300, function() {
                 $(this).css('display', '');
@@ -162,13 +174,16 @@
 
             $sidebar_content.find('li.active').parents('li').addClass('open');
             $listItem.addClass('open').removeClass('nav-collapsed-open');
+            console.log('collapse');
         }
     }).on('mouseleave', function(event) {
         if($wrapper.hasClass('nav-collapsed')){
             $wrapper.addClass('menu-collapsed');
+            $('#logo2').hide();
             var $listItem = $('.navigation li.open'),
             $subList = $listItem.children('ul');
             $listItem.addClass('nav-collapsed-open');
+            console.log('collapse1');
 
             $subList.show().slideUp(300, function() {
                 $(this).css('display', '');
@@ -225,7 +240,7 @@
         $sidebar.addClass('hide-sidebar');
     });
 
-    $('.noti-list').perfectScrollbar();
+    // $('.noti-list').perfectScrollbar();
 
 
     // Page full screen
@@ -249,5 +264,8 @@
         }
     }
 
+    $('#hamburger').click(function () {
+        $('#logo2').show();
+    });
 
 });
